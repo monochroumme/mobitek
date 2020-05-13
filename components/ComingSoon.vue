2eb555ef58b21babddc9d692d9f38be93801dd3b
rare <chipchroumme@gmail.com>
2020-05-13 08:48:23 +0400

    coming-soon page is ready

---
+++ components/ComingSoon.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="coming-soon-page">
+		<img class="logo" src="~/static/pics/svg/logo.svg">
+		<h1>Tezliklə xidmətinizdə!</h1>
+		<img class="bg" src="~/static/pics/svg/coming-soon.svg">
+		<img class="bg-small" src="~/static/pics/svg/coming-soon-small.svg">
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index-page">
+    <ComingSoon />
+  </div>
+</template>
+
+<script>
+import ComingSoon from '~/components/ComingSoon';
+
+export default {
+  components: {
+    ComingSoon
+  }
+}
+</script>
